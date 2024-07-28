9b69d77564b0fd1fea378a8242fa3aa15017ecf1
MhmmdIchsan <mhichsan21@mhs.usk.ac.id>
2024-07-29 00:55:20 +0700

    feat: modify permission on spatie

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -12,6 +12,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules;
 use Illuminate\View\View;
+use App\Models\Bidang;
 
 class RegisteredUserController extends Controller
 {
@@ -20,7 +21,8 @@ class RegisteredUserController extends Controller
      */
     public function create(): View
     {
-        return view('auth.register');
+        $bidang = Bidang::all();
+        return view('auth.register', compact('bidang'));
     }
 
     /**
@@ -33,12 +35,18 @@ class RegisteredUserController extends Controller
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
+            'phone' => ['required', 'string', 'max:255', 'unique:'.User::class],
+            'nip_reg' => ['required', 'string', 'max:255', 'unique:'.User::class],
+            'bidang_id' => ['required', 'integer'],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
+            'phone' => $request->phone,
+            'nip_reg' => $request->nip_reg,
+            'bidang_id' => $request->bidang_id,
             'password' => Hash::make($request->password),
         ]);
 
