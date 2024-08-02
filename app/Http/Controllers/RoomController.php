5ba71516f9412cc5b70d15eca033f1304993c5ac
Ardian <ardian_21@mhs.unsyiah.ac.id>
2024-08-02 11:14:43 +0700

    feat: add user room data

--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -2,51 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Ruang;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class RoomController extends Controller
 {
     public function index()
-    {
-        $rooms = [
-            [
-                'title' => 'RUANG PIMPINAN A105',
-                'image' => 'https://plus.unsplash.com/premium_photo-1689701711379-154c21998787?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cnVhbmdhbiUyMHJhcGF0fGVufDB8fDB8fHww',
-                'details' => [
-                    'Lokasi' => 'Gedung Kampus A',
-                    'Lantai' => '1',
-                ],
-                'detailsUrl' => '/rooms/a105',
-            ],
-            [
-                'title' => 'RUANG PIMPINAN A105',
-                'image' => 'https://plus.unsplash.com/premium_photo-1689701711379-154c21998787?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cnVhbmdhbiUyMHJhcGF0fGVufDB8fDB8fHww',
-                'details' => [
-                    'Lokasi' => 'Gedung Kampus A',
-                    'Lantai' => '1',
-                ],
-                'detailsUrl' => '/rooms/a106',
-            ],
-            [
-                'title' => 'RUANG PIMPINAN A105',
-                'image' => 'https://plus.unsplash.com/premium_photo-1689701711379-154c21998787?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cnVhbmdhbiUyMHJhcGF0fGVufDB8fDB8fHww',
-                'details' => [
-                    'Lokasi' => 'Gedung Kampus A',
-                    'Lantai' => '1',
-                ],
-                'detailsUrl' => '/rooms/a106',
-            ],
-            [
-                'title' => 'RUANG PIMPINAN A105',
-                'image' => 'https://plus.unsplash.com/premium_photo-1689701711379-154c21998787?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cnVhbmdhbiUyMHJhcGF0fGVufDB8fDB8fHww',
-                'details' => [
-                    'Lokasi' => 'Gedung Kampus A',
-                    'Lantai' => '1',
-                ],
-                'detailsUrl' => '/rooms/a106',
-            ],
-        ];
+    {     
+        $rooms = Ruang::all();
 
+        foreach($rooms as $room){
+            $room->image_url = Storage::url($room->image);
+        }
         return view('pages.ruangan', compact('rooms'));
     }
 }
--- resources/views/components/room-card.blade.php
+++ resources/views/components/room-card.blade.php
@@ -1,13 +1,14 @@
 <!-- resources/views/components/room-card.blade.php -->
 <div class="max-w-sm rounded-xl overflow-hidden shadow-lg bg-white transition-transform duration-300 hover:scale-105">
-    <img class="w-full h-48 object-cover" src="{{ $room['image'] }}" alt="{{ $room['title'] }}">
-    <div class="px-6 py-4">
-        <div class="font-bold text-xl mb-2 text-gray-700 hover:text-gray-300 transition duration-300 ease-in-out">{{ $room['title'] }}</div>
-        @foreach($room['details'] as $key => $value)
-            <p class="text-gray-700 text-base">
-                {{ $key }}: {{ $value }}
-            </p>
-        @endforeach
+    <img class="w-full h-48 object-cover" src="{{ $room->image_url }}" alt="{{ $room->nama }}">
+    <div class="px-6 py-2">
+        <div class="font-bold text-xl mb-2 text-gray-700 hover:text-gray-300 transition duration-300 ease-in-out">{{ $room->nama }}</div>
+        <p>
+            Kepemilikan: {{ $room->bidang->nama }}
+        </p>
+        <p>
+            {{ $room->lokasi}}
+        </p>
     </div>
     <div class="px-6 pt-4 pb-2">
         <a href="/ruangan-detail" class="bg-gray-700 hover:bg-gray-300 transition duration-300 ease-in-out text-white font-bold py-2 px-4 rounded-lg inline-block">
