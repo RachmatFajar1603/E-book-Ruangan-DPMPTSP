2bb14ef2249a38eea966fa4f5f5deb6a8126d124
Rachmat Fajar <rachmat3@mhs.usk.ac.id>
2024-08-06 17:04:09 +0700

    feat: Add pagination and search functionality to Laporan component

--- app/Livewire/Admin/Laporan.php
+++ app/Livewire/Admin/Laporan.php
@@ -7,12 +7,19 @@ use Livewire\Component;
 use App\Models\Peminjaman;
 use App\Exports\PeminjamanExport;
 use Maatwebsite\Excel\Facades\Excel;
+use Livewire\WithPagination;
 
 class Laporan extends Component
 {
+    use WithPagination;
+
     #[Title('Laporan')]
     public $startDate;
     public $endDate;
+    public $search = '';
+    public $perPage = 10;
+
+    protected $queryString = ['search' => ['except' => '']];
 
     public function mount()
     {
@@ -29,18 +36,36 @@ class Laporan extends Component
     public function getPeminjaman()
     {
         return Peminjaman::whereBetween('tanggal_pinjam', [$this->startDate, $this->endDate])
+            ->where(function ($query) {
+                $query->where('penanggung_jawab', 'like', '%' . $this->search . '%')
+                    ->orWhereHas('user', function ($subQuery) {
+                        $subQuery->where('nama', 'like', '%' . $this->search . '%')
+                            ->orWhere('nip_reg', 'like', '%' . $this->search . '%');
+                    })
+                    ->orWhereHas('ruangan', function ($subQuery) {
+                        $subQuery->where('nama', 'like', '%' . $this->search . '%');
+                    })
+                    ->orWhere('catatan', 'like', '%' . $this->search . '%');
+            })
             ->orderBy('tanggal_pinjam')
-            ->get();
+            ->paginate($this->perPage);
     }
 
     public function exportExcel()
     {
-        return Excel::download(new PeminjamanExport($this->startDate, $this->endDate), 'laporan_peminjaman.xlsx');
+        return Excel::download(new PeminjamanExport($this->startDate, $this->endDate, $this->search), 'laporan_peminjaman.xlsx');
     }
 
     public function clearFilter()
     {
         $this->startDate = now()->startOfMonth()->format('Y-m-d');
         $this->endDate = now()->endOfMonth()->format('Y-m-d');
+        $this->search = '';
+        $this->resetPage();
+    }
+
+    public function updatingSearch()
+    {
+        $this->resetPage();
     }
 }
