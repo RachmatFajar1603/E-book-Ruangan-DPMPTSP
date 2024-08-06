e2d061c781d8a8161097f5f021845ae8aab4dd0a
Rachmat Fajar <rachmat3@mhs.usk.ac.id>
2024-08-06 22:50:31 +0700

    fix: Make catatan field required in pinjam-ruangan-book.blade.php

--- resources/views/livewire/admin/pinjam-ruangan-book.blade.php
+++ resources/views/livewire/admin/pinjam-ruangan-book.blade.php
@@ -115,7 +115,7 @@
                         class="text-start block mb-2 text-sm font-medium text-gray-900 dark:text-white">Tambah
                         pesan</label>
                     <textarea wire:model="catatan" rows="4"
-                        class="h-48 block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></textarea>
+                        class="h-48 block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required></textarea>
                 </div>
                 <button type="submit"
                     class="my-8 text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
